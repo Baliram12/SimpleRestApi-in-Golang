3bfbc2c81192c8632177b292cf2394f77915b97c
Baliram12 <balichauhan1997@gamil.com>
2020-10-19 11:25:07 +0530

    RestApi in Golang

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type server struct{}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "hello Baliram"}`))
+	w.Write([]byte(`{"message": "Welcome to Golang Programming"}`))
+}
+
+func main() {
+	s := &server{}
+	http.Handle("/", s)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
